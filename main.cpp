8150e4a4eeabf6cfadabcdee0bd9578c742822db
Cezary Suchorski <cezary.suchorski@wp.pl>
2022-02-28 20:01:13 +0100

    Added randTab function, changed tab to a dynamic type

--- main.cpp
+++ main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
-void BubbleSort(int tab[]);
-void PrintTab(int tab[]);
+void BubbleSort(int tab[], int size);
+void PrintTab(int tab[], int size);
 void Swap(int* a, int* b);
+void RandTab(int tab[], int size);
 
 int main()
 {
-	int tab[10]{ 13,28,1,7,6,5,4,1,2,10 };
-	PrintTab(tab);
-	BubbleSort(tab);
-	PrintTab(tab);
+	const int size = 10;
+	int* tab = new int[size];
+	RandTab(tab, size);
+	PrintTab(tab, size);
+	BubbleSort(tab, size);
+	PrintTab(tab, size);	
+
 	return 0;
 }
 
-void BubbleSort(int tab[]) 
+void BubbleSort(int tab[], int size) 
 {
-	for (int i = 0; i < 9; i++)
+	for (int i = 0; i < size - 1; i++)
 	{
-		for (int j = 0; j < 9; j++)
+		for (int j = 0; j < size - 1 - i; j++)
 		{
 			if (tab[j] > tab[j + 1])
 			{
@@ -28,10 +33,9 @@ void BubbleSort(int tab[])
 	}
 }
 
-
-void PrintTab(int tab[])
+void PrintTab(int tab[], int size)
 {
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < size; i++)
 	{
 		cout << tab[i] << endl;
 	}
@@ -43,4 +47,13 @@ void Swap(int* a, int* b)
 	int tmp = *a;
 	*a = *b;
 	*b = tmp;
+}
+
+void RandTab(int tab[], int size)
+{
+	srand(time(NULL));
+	for (int i = 0; i < size; i++)
+	{
+		tab[i] = rand() % 20 + 1;
+	}
 }
