da86c3018945791141b9347b09942ec19f809a10
Cezary Suchorski <cezary.suchorski@wp.pl>
2022-03-22 22:36:48 +0100

    Szukanie z wartownikiem, szukanie lidera

--- main.cpp
+++ main.cpp
@@ -5,16 +5,19 @@ using namespace std;
 void BubbleSort(int tab[], int size);
 void SelectionSort(int tab[], int size);
 void InsertSort(int tab[], int size);
+void CountingSort(int tab[], int size, int max);
 void SitoEratostenesa(int max);
 bool IsPrime(int num);
 void PrintTab(int tab[], int size);
 void Swap(int* a, int* b);
+void SzukanieLidera(int tab[], int size);
 void RandTab(int tab[], int size);
 
 
 int main()
 {
-	SitoEratostenesa(20);
+	int tab[10] = { 1,2,3,4,5,6,7 };
+	CountingSort(tab, 10, 7);
 	return 0;
 }
 
@@ -58,6 +61,20 @@ void InsertSort(int tab[], int size)
 			key--;
 		}
 	}
+}
+
+void CountingSort(int tab[], int size, int max)
+{
+	int* countTab = new int[max + 1]{0};
+	for (int i = 0; i < size; i++)
+	{
+		countTab[tab[i]]++;
+	}
+	for (int i = 1; i <= size; i++)
+	{
+		countTab[i] += countTab[i - 1];
+	}
+
 }
 
 void PrintTab(int tab[], int size)
@@ -124,4 +141,44 @@ bool IsPrime(int num)
 		}
 	}
 	return true;
+}
+
+bool LinearSearch(int tab[], int size, int value)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (tab[i] == value) return true;
+	}
+	return false;
+}
+
+bool SzukanieZWartownikiem(int tab[], int size, int value)
+{
+	tab[size] = value;
+	int i;
+	for (i = 0; i <= size; i++)
+	{
+		if (tab[i] == value) break;
+	}
+	if (i < size) return true;
+	return false;
+}
+
+void SzukanieLidera(int tab[], int size)
+{
+	int count = 0;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (tab[j] == tab[i]) count++;
+		}
+		if (count > size / 2)
+		{
+			cout << "Liderem jest: " << tab[i] << endl;
+			break;
+		}
+		count = 0;
+	}
+	cout << "Zbior nie ma lidera\n";
 }
